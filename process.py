a8f4ba59eae3815123644a55d839015dea60d405
ZHAN-GAN <107039598+ZHAN-GAN@users.noreply.github.com>
2022-06-13 12:20:24 +0900

    Update process.py

--- process.py
+++ process.py
@@ -2,85 +2,68 @@
 from dipy.io.image import load_nifti, save_nifti
 from dipy.denoise.nlmeans import nlmeans
 from dipy.denoise.noise_estimate import estimate_sigma
-import os
-
 import SimpleITK as sitk
-
 import numpy as np
-import os
 import nibabel as nib
 from nilearn.image import resample_img
 
-'''
-在预处理前，需要注意到的，mask数组里面只能存在0或者1这两种值，不然在bias field correction阶段会出问题
-主要是以下几个步骤：
-1.denoise
-2.bias field correction
-3.resample
-4.z-score
-输出的就是预处理之后的图像(nii格式)和mask（nrrd格式）
-'''
 
 '''
-这里只需要输入image就可以
-'''
-def denoise_mri(root_path, save_path):
-    phase_name_list = ['pv', 'art', 't2', 'dwi1']
-    for stage in ['test', 'train']:
-        for fold_mark in ['image']:
-            for pn in phase_name_list:
-                filenames = os.listdir(root_path + pn + '/'+stage+'/'+fold_mark+'/')
-                for fn in filenames:
-                    data, affine = load_nifti(root_path + pn + '/'+stage+'/'+fold_mark+'/'+fn)
-                    sigma = estimate_sigma(data)
-
-                    den = nlmeans(data, sigma)
-                    save_dir = save_path+ pn + '/'+stage+'/'+fold_mark+'/'
-                    if not os.path.exists(save_dir):
-                        os.makedirs(save_dir)
-                    save_nifti(save_dir+fn, den, affine)
-
+e.g. 
+image_path='/root/mri/raw/image/demo_image.nii'
+mask_path='/root/mri/raw/image/demo_image.nrrd'
+save_image_path='/root/mri/preprocess/image/demo_image.nii'
+save_mask_path='/root/mri/preprocess/image/demo_image.nrrd'
 
 '''
-这里需要输入image和对应的mask
-'''
-def N4_bias_field_correction_tumor_mask(image_path, mask_path, save_image_path, save_mask_path):
+def process(image_path, mask_path, save_image_path, save_mask_path):
 
-    image_file = sitk.ReadImage(image_path, sitk.sitkFloat32)
+    # denise image
+    data, affine = load_nifti(image_path)
+    sigma = estimate_sigma(data)
+    den = nlmeans(data, sigma)
+    save_nifti('temp_image.nii', den, affine)
+
+    # N4 bias field correction
+    image_file = sitk.ReadImage('temp_image.nii', sitk.sitkFloat32)
     mask_file = sitk.ReadImage(mask_path, sitk.sitkUInt8)
 
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
     correct_image = corrector.Execute(image_file, mask_file)
-    sitk.WriteImage(correct_image, save_image_path)
-    sitk.WriteImage(mask_file, save_mask_path)
 
+    sitk.WriteImage(correct_image, 'temp_image.nii')
+    sitk.WriteImage(mask_file, 'temp_mask.nii')
 
-'''
-这里的处理对mask文件有改动
-'''
-def resample_mri(image_path, save_image_path, mask_path, save_mask_path):
-    temp_file = nib.load(image_path)
-    new_file = resample_img(temp_file, target_affine=np.eye(3), interpolation='linear')
-    nib.save(new_file, save_image_path)
+    # resampling
+    temp_file = nib.load('temp_image.nii')
+    new_file = resample_img(temp_file, target_affine=np.eye(3), interpolation='nearest')
+    nib.save(new_file, 're_image.nii')
 
-    temp_file = nib.load(mask_path)
-    new_file = resample_img(temp_file, target_affine=np.eye(3), interpolation='linear')
-    nib.save(new_file, save_mask_path)
+    mask_file = nib.load('temp_mask.nii')
+    new_file = resample_img(mask_file, target_affine=np.eye(3), interpolation='nearest')
+    nib.save(new_file, 're_mask.nii')
 
-
-'''
-对整个volume进行的z_score
-'''
-def zscore_volume(image_path, mask_path, save_image_path, save_mask_path):
-    image = sitk.ReadImage(image_path, sitk.sitkFloat32)
-    mask = sitk.ReadImage(mask_path, sitk.sitkUInt8)
+    # normalization
+    image = sitk.ReadImage('re_image.nii', sitk.sitkFloat32)
+    mask = sitk.ReadImage('re_mask.nii', sitk.sitkUInt8)
 
     image_array = sitk.GetArrayFromImage(image)
-    mask_array = sitk.GetArrayFromImage(mask)
 
     mean_value = np.mean(image_array)
     std_value = np.std(image_array)
     new_image_array = (image_array - mean_value) / std_value
 
-    np.save(save_image_path, new_image_array)
-    np.save(save_mask_path, mask_array)
+    new_image = sitk.GetImageFromArray(new_image_array,
+                                       isVector=image.GetNumberOfComponentsPerPixel() > 1)
+    new_image.SetOrigin(image.GetOrigin())
+    new_image.SetSpacing(image.GetSpacing())
+    new_image.SetDirection(image.GetDirection())
+
+    sitk.WriteImage(new_image, save_image_path)
+    sitk.WriteImage(mask, save_mask_path)
+
+
+process(image_path='1148333_4322223_BH_Ax_LAVA+C_4_0_2_axial_PV.nii',
+        mask_path='1148333_4322223_BH_Ax_LAVA+C_4_0_2_axial_PV.nrrd',
+        save_image_path='image.nii',
+        save_mask_path='mask.nrrd')
